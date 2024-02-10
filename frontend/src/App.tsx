8ad6090d476a6d49087ccb22c4abf94680a8b814
jimmyfrysinger <jimmyfrysinger@gmail.com>
2024-02-09 21:09:09 -0500

    Install tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,15 @@
 import { FC, useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { fetchTestData } from "./services/testService";
 
 const App: FC = () => {
-  const [testData, setTestData] = useState<string>();
+  const [helloData, setHelloData] = useState();
 
   useEffect(() => {
     const getHelloData = async () => {
       try {
         const data = await fetchTestData();
-        setTestData(data.message);
+        setHelloData(data.message);
       } catch (error) {
         console.warn(error);
       }
@@ -18,24 +17,7 @@ const App: FC = () => {
     getHelloData();
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React {testData}
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="">{helloData ?? "Error fetching data"}</div>;
 };
 
 export default App;
--- frontend/src/services/testService.ts
+++ frontend/src/services/testService.ts
@@ -1,6 +1,6 @@
 export const fetchTestData = async () => {
   try {
-    const response = await fetch("//localhost:3001/api/hellsfgo");
+    const response = await fetch("//localhost:3001/api/hello");
     if (!response.ok) {
       throw new Error("Failed to fetch data from endpoint.");
     }
